bbd8a4f51b51df2c743af97ae10ffa03bc3b8ffc
Jeremy Jackson <jjacks5180@gmail.com>
2023-04-04 10:26:26 -0700

    update password generator

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "1234567890";
+var characters ="!@#$%^&*()?";
 
 function generatePassword() {
   // var passwordText = document.querySelector("#password");
   //passwordText.value = "some word";
   // return passwordText.value;
   var lengthOption = prompt("Choose a length between 8 and 128 characters");
-  var typeOption = prompt(
-    "Would you like lowercase, uppercase, numeric, or special characters?"
-  );
+  
+  var lowercaseOption = confirm("Would you like lowercase?");
+  var uppercaseOption = confirm("Would you like uppercase?");
+  var numericOption = confirm("Would you like numeric?");
+  var characterOption = confirm("Would you like characters?");
 }
 // Write password to the #password input
 function writePassword() {
